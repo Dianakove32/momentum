6f4f63fd163ae2f57e471592fb892c60c9f44280
gortatka <gtatsiana@tut.by>
2021-03-20 22:23:27 +0300

    change clock

--- src/components/clock/clock.js
+++ src/components/clock/clock.js
@@ -4,26 +4,65 @@ import './clock.scss';
 class Clock extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { time:new Date().toLocaleString() }
+
+        this.state = {
+          timer: setInterval(this.setDate.bind(this), 1000),
+          secondDegrees: 0,
+          minuteDegress: 0,
+          hourDegrees: 0
+        }
     }
-    componentDidMount() {
-        this.intervalID = setInterval(() => this.tick(), 1000);
-      }
-      componentWillUnmount() {
-        clearInterval(this.intervalID);
-      }
-      tick() {
-        this.setState({
-          time: new Date().toLocaleString(),
-        });
-      }
+
+    componentWillUnmount() {
+        clearInterval(this.state.timer);
+    }
+
+    setDate() {
+      const deg = 6;
+      const hr = document.querySelector('#hr');
+      const mn = document.querySelector('#mn');
+      const sc = document.querySelector('#sc');
+      
+      setInterval(() =>{
+          
+           let day = new Date();
+      let hh = day.getHours() * 30;
+      let mm = day.getMinutes() * deg;
+      let ss = day.getSeconds() * deg;
+      
+      hr.style.transform = `rotateZ(${(hh)+(mm/12)}deg)`;
+      mn.style.transform = `rotateZ(${mm}deg)`;
+      sc.style.transform = `rotateZ(${ss}deg)`;
+      });
+    }
     render() {
+      const secondStyle = {
+          transform: `rotate(${this.state.secondDegrees}deg)`
+      };
+      const minuteStyle = {
+          transform: `rotate(${this.state.minuteDegrees}deg)`
+      };
+      const hourStyle = {
+          transform: `rotate(${this.state.hourDegrees}deg)`
+      };
         return (
-           <p className = "App-clock" >
-           { this.state.time } </p>
-        );
+            <div class="clock">
+
+            <div class="hour"style={hourStyle}>
+                <div class="hr" id="hr"></div>
+            </div>
+    
+            <div class="min" style= {minuteStyle} >
+                <div class="mn" id="mn"></div>
+            </div>
+    
+            <div class="sec"style= {secondStyle} >
+                <div class="sc" id="sc"></div>
+            </div>
+    
+        </div>
+        )
     }
 }
-
 
 export default Clock;
