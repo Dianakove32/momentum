47265ae5769a9da8c0a2eefa7aba2ef82eb9093d
gortatka <gtatsiana@tut.by>
2021-04-07 14:32:58 +0300

    add API news

---
+++ src/components/news/cardNew.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default function CardNew({title,description,url,urlToImage}) {
+    
+  return (
+
+    <div className="cardNew">
+    
+      
+        <h1>{title}</h1>
+       <p>{description}</p>
+       <a href={url}>bgbfg</a>
+       
+      
+     
+    </div>
+
+
+
+  )
+
+}
--- src/components/news/news.js
+++ src/components/news/news.js
@@ -1,11 +1,38 @@
+
 import React from 'react';
+import CardNew from './cardNew';
+import { useContext } from 'react';
+import {ApiContext} from '../../context/ApiContext';
 
-export default class News extends React.Component{
-render(){
-return(
-        <div>
-            <div>hello news</div>
-            <iframe title="news"  width="250" height="750" class="rssdog" src="https://www.rssdog.com/index.php?url=http%3A%2F%2Fnews.tut.by%2Frss%2Findex.rss&mode=html&showonly=&maxitems=0&showdescs=1&desctrim=0&descmax=0&tabwidth=100%25&utf8=1&linktarget=_blank&bordercol=%23d4d0c8&headbgcol=%23999999&headtxtcol=%23ffffff&titlebgcol=%23f1eded&titletxtcol=%23000000&itembgcol=%23ffffff&itemtxtcol=%23000000&ctl=0"></iframe>
+const News = () => {
+    const context=useContext(ApiContext);
+   
+    return (
+       <div>
+           {console.log(context.data)}
+        {
+            context.data.articles.map((el) => {
+              return (
+                <CardNew
+                  key={el.title}
+                  
+                  title={el.title}
+                  description={el.description}
+                  url={el.url}
+                />
+                );
+            })}
         </div>
+    )
+}
+export default News;
 
-)}}
+
+
+
+
+
+
+
+
+
