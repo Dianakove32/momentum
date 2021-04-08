c84260a000cbdbef57eae8806e224d002b8a7d1c
Dziana Kavaleva <dianakove32@gmail.com>
2021-04-08 13:09:22 +0300

    add layout to main page

--- src/home.js
+++ src/home.js
@@ -1,6 +1,7 @@
 
 import React from 'react';
 import './App.scss';
+import './components/home/homeStyle.scss'
 import Clock from './components/clock/clock';
 import Randomizer from './components/randomizer/Randomizer'
 import Form from './components/weather/Form';
@@ -13,84 +14,111 @@ import { NavLink } from "react-router-dom";
 import News from './components/news/news'
 import Dish from './components/dish/dishRandom'
 import Currency from './components/currency/currency'
-const API_KEY= '8768da57bd891fa41359848c1665c9e4';
+import Icon from './components/icon/Icon';
+
+const API_KEY = '8768da57bd891fa41359848c1665c9e4';
 
 
 
 class Home extends React.Component {
 
-  state={
+  state = {
 
-    icon:undefined,
+    icon: undefined,
     temp: undefined,
     city: undefined,
-    country:undefined ,
+    country: undefined,
     humidity: undefined,
     sunset: undefined,
     description: '...',
   }
-  gettingWeather= async (e) =>{
+  gettingWeather = async (e) => {
     e.preventDefault();
-    const city=e.target.elements.city.value;
-console.log(  'xbfc', city)
-    if(city){
+    const city = e.target.elements.city.value;
+    console.log('xbfc', city)
+    if (city) {
       const api_url = await
         fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=8768da57bd891fa41359848c1665c9e4&units=metric`)
       const data = await api_url.json()
       console.log(data)
 
-      var sunset=data.sys.sunset;
+      var sunset = data.sys.sunset;
 
       var date = new Date();
       date.setTime(sunset);
-      var sunset_date = date.getHours()+ ":" + date.getMinutes()+ ":" + date.getSeconds()
+      var sunset_date = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds()
 
 
 
       this.setState({
 
         city: data.name,
-        temp: Math.ceil(data.main.temp)+ '°C',
-      // icon:data.weather[0].icon,
-        country:data.sys.country,
+        temp: Math.ceil(data.main.temp) + '°C',
+        // icon:data.weather[0].icon,
+        country: data.sys.country,
         humidity: data.main.humidity,
-        sunset:  sunset_date,
+        sunset: sunset_date,
         description: data.weather[0].description
       })
     }
-  
+
   }
-          
 
 
 
 
-  render(){
-      const  gettingWeather =this.gettingWeather
-      const {city, country,temp,icon,sunset,humidity,description, }=this.state
+
+  render() {
+    const gettingWeather = this.gettingWeather
+    const { city, country, temp, icon, sunset, humidity, description, } = this.state
     return (
       <div className='App'>
-       
-      <div className='wrapper'>
-           <div className='weatherBlock'>
-         <Form   data={data} gettingWeather={gettingWeather}/>
-        <Weather  city={city}
-        country ={country}
-        temp = {temp}
-        sunset = {sunset}
-        icon={icon}
-        humidity={humidity}
-        description={description}
-         data={data} />
-      </div>
 
-      <Clock />
+        <div className='layout-2-column'>
+        <div className="todo-container">
+           <ToDo />
+        </div>
+        <div className="icon-container">
+        <Currency />
+           <Icon/>
+        </div>
+        </div>
+        <div className='layout-3-column'>
+        <div className="news-container">NEWS</div>
+        <div className="clock-container">
+          <Clock />
+        </div>
+        <div className="dish-container">
+           DISH
+        </div>
+        </div>
+        <div className='layout-2-column'>
+        <div className="weather-container">
+             <div className='weatherBlock'>
+            <Form data={data} gettingWeather={gettingWeather} />
+            <Weather city={city}
+              country={country}
+              temp={temp}
+              sunset={sunset}
+              icon={icon}
+              humidity={humidity}
+              description={description}
+              data={data} />
+          </div>
+        </div>
+        <div className="quote-container">
+             <Randomizer />
+        </div>
+        </div>
 
-      </div>          
-        <Randomizer />       
-        <ToDo/>
-       <Currency/> 
-       <Dish/>
+
+
+
+
+
+
+
+
       </div>
     );
   }
