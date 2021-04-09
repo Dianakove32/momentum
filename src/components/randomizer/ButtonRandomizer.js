58979699980792f07d8bdca0226a82d969e2d59b
Dziana Kavaleva <dianakove32@gmail.com>
2021-04-09 19:47:57 +0300

    feat: add style dish cont

--- src/components/randomizer/ButtonRandomizer.js
+++ src/components/randomizer/ButtonRandomizer.js
@@ -9,7 +9,7 @@ class ButtonRandomizer extends React.Component {
     render() {
       return(
         <div className='buttons'>
-          <a id='tweet-quote'   className='button' href={`https://twitter.com/intent/tweet/?text=${this.props.quote} - ${this.props.author}`}><i className='fab fa-twitter'></i></a>
+
           <button   id='new-quote' className='button floating-button' onClick={this.props.handleClick}>
             New quote
           </button>
