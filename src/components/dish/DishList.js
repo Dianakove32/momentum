c6ef18c4ce7d0d4a0b0859c85322d42cc628b83e
LapitskayaN <lapitskaya.nl@gmail.com>
2021-04-17 16:39:21 +0300

    try cookies

--- src/components/dish/DishList.js
+++ src/components/dish/DishList.js
@@ -4,28 +4,13 @@ import { ApiContext } from "../../components/context/Context";
 import Dish from "./Dish.js";
 import Favorites from "./Favorites";
 import "./style.scss"
+import {setCookie,  getCookie , removeCookie} from "../../services/cookies";
 
-import MuiAlert from '@material-ui/lab/Alert';
-import { makeStyles } from '@material-ui/core/styles';
-import Snackbar from '@material-ui/core/Snackbar';
-
-function Alert(props) {
-    return <MuiAlert elevation={6} variant="filled" {...props} />;
-  }
-
-  const useStyles = makeStyles((theme) => ({
-    root: {
-      width: '100%',
-      '& > * + *': {
-        marginTop: theme.spacing(2),
-      },
-    },
-  }));
 export default function DishList(props) {
 
     const context = useContext(ApiContext);
-    const classes = useStyles();
-    const [open, setOpen] = React.useState(false);
+  
+
     const onChange = (e) => {
         context.setSearch(e.target.value)
     }
@@ -33,18 +18,14 @@ export default function DishList(props) {
         const item = context.state.data.data.hits.find(el => el.recipe.label == title)
         let copyOfItems = [...context.state.cart]
         copyOfItems.push(item)
+        setCookie(copyOfItems)
+
         context.setState({
             ...context.state,
             cart: copyOfItems
         })
-        setOpen(true);
     }
-    const handleClose = (event, reason) => {
-        if (reason === 'clickaway') {
-          return;
-        }
-        setOpen(false);
-      };
+   
 
     return (
         <div className='dish-wrapper'>
@@ -57,7 +38,7 @@ export default function DishList(props) {
             </div>
 
             </div>
-<h2>Find dish for today</h2>
+                <h2>Find dish for today</h2>
             <div className="input-wrapper">
                  <input className="input-dish" type='text' placeholder='Find recipy...' autoComplete='on' onChange={onChange} />
             </div>
@@ -66,16 +47,6 @@ export default function DishList(props) {
                 {context.state.isLoaded &&
                     context.state.data.data.hits.map((el, i) => <Dish key={i} {...el.recipe} onClick={onClick} />)}
             </div>
-            <div className={classes.root}>
-
-      <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
-         <Alert onClose={handleClose} severity="success">
-         Recipy has been added
-        </Alert>
-      </Snackbar>
-
-      <Alert severity="success">Recipy has been added </Alert>
-    </div>
         </div>
     )
 }
--- src/components/dish/dishRandom.js
+++ src/components/dish/dishRandom.js
@@ -1,52 +1,55 @@
-// import React from 'react';
+import React from 'react';
 
 
-// class Dish extends React.Component {
-//   constructor(props) {
-//     super(props);
+class Dish extends React.Component {
+  constructor(props) {
+    super(props);
 
-//     this.state = {
-//       uri:"",
-//       label:"",
-//       image:""
-//           }
+    this.state = {
+      uri:"",
+      label:"",
+      image:""
+          }
+    
+          this.getRecipe = this.getRecipe.bind(this);
+  }
 
-//           this.getRecipe = this.getRecipe.bind(this);
-//   }
+  getRecipe() {
+    const API = 'https://api.edamam.com/search?q=chicken&app_id=02ff5b94&app_key=38d7e7e23122d500b9bfd3beced06c29&from=0&to=3&calories=591-722&health=alcohol-free'
+    fetch(API)
+        .then((response) =>{ response.json();
+        console.log(response)})
+        
+        .then((data) => {
+          console.log(data)
+            this.setState({
+              uri:data.uri, image:data.image, label:data.label 
+             
+            })
+       
+        })
+        .catch(error => console.log('Error', error));
+        
 
-//   getRecipe() {
-//     const API = 'https://api.edamam.com/search?q=chicken&app_id=02ff5b94&app_key=38d7e7e23122d500b9bfd3beced06c29&from=0&to=3&calories=591-722&health=alcohol-free'
-//     fetch(API)
-//         .then((response) =>{ response.json();
-//         console.log(response)})
+  }
+    
+  render() {
+    return (
+      <div>
+     Hello dish from Diana!
+      <button onClick={this.getRecipe}>dish</button> 
+      </div>
+       
+     
+    )
+  }
+}
 
-//         .then((data) => {
-//           console.log(data)
-//             this.setState({
-//               uri:data.uri, image:data.image, label:data.label
-
-//             })
-
-//         })
-//         .catch(error => console.log('Error', error));
-
-
-//   }
-
-//   render() {
-//     return (
-//       <div>
-//      Hello dish from Diana!
-//       <button onClick={this.getRecipe}>dish</button>
-//       </div>
-
-
-//     )
-//   }
-// }
-
-
+    
+    
 
 
+export default Dish;
 
-// export default Dish;
+
+
