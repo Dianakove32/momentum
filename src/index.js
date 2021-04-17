568b7dd29b3c030f4bb35c37fb5348f7d03f31d3
LapitskayaN <lapitskaya.nl@gmail.com>
2021-04-17 15:26:55 +0300

    cookies try

--- src/index.js
+++ src/index.js
@@ -5,16 +5,19 @@ import App from './App';
 import { HashRouter } from "react-router-dom";
 import ContextProvider from './components/context/Context';
 import ApiProvider from "./context/ApiContext";
+import { CookiesProvider } from 'react-cookie';
 
 
 ReactDOM.render(
   <HashRouter >
     <ApiProvider>
-  <ContextProvider>
-     <App />
-  </ContextProvider>
-</ApiProvider>
-    </HashRouter>,
+      <ContextProvider>
+        <CookiesProvider>
+         <App />
+        </CookiesProvider>
+      </ContextProvider>
+    </ApiProvider>
+  </HashRouter>,
   document.getElementById('root')
 );
 
