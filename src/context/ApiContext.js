2259f7c3a8e77e8eaaa7a4e6a3f4b6430383a937
gortatka <gtatsiana@tut.by>
2021-04-20 21:01:54 +0300

    other news

--- src/context/ApiContext.js
+++ src/context/ApiContext.js
@@ -2,22 +2,27 @@
 import {createContext,useState,useEffect} from "react";
 import moment from 'moment';
 export const ApiContext=createContext();
-let now = moment().format();
-let newNow=now.substring(0, now.length - 15);
+// let now = moment().format();
+// let newNow=now.substring(0, now.length - 15);
 const ContextProvider=({children})=>{
     const [data, setData] = useState({});
     
     useEffect(() => {
         // eslint-disable-next-line no-template-curly-in-string
-        const url = `https://newsapi.org/v2/everything?q=from=${newNow}&keyword&apiKey=0e428c7efd55415aba1f5a8b3fc39521`;
-console.log(newNow)
-
-        fetch(url)
+        fetch("https://bing-news-search1.p.rapidapi.com/news?safeSearch=Off&textFormat=Raw", {
+	"method": "GET",
+	"headers": {
+		"x-bingapis-sdk": "true",
+		"x-rapidapi-key": "882af9aa22mshb2364291efb3859p188c75jsnb2edc39e47b4",
+		"x-rapidapi-host": "bing-news-search1.p.rapidapi.com"
+	}
+})
             .then(data => data.json())
             .then(res => setData(res))
 
     }, [])
     return(
+      
        <ApiContext.Provider value={{data,setData}}>
            {children}
        </ApiContext.Provider>
