c9b31b2a21efdd55d9b72fe0e30b06f911b50e57
gortatka <gtatsiana@tut.by>
2021-04-22 23:38:15 +0300

    add data

---
+++ src/components/news/data.js
@@ -0,0 +1,33 @@
+const news=[
+    {
+        name:"Olaf Scholz: Leise, aber nicht leise genug",
+        image:"https://www.msn.com/de-de/finanzen/top-stories/olaf-scholz-leise-aber-nicht-leise-genug/ar-BB1fWhL1",
+        url:"https://www.msn.com/de-de/finanzen/top-stories/olaf-scholz-leise-aber-nicht-leise-genug/ar-BB1fWhL1"
+    },
+    {
+        name:"Virusvariante bereits in Europa nachgewiesen: Was über die indische Corona-Mutante B.1.617 bekannt ist",
+        image:"https://www.bing.com/th?id=OVFT.TvjbugkscOJMEWJiGGmEpy&amp;pid=News",
+        url:"https://www.msn.com/de-de/nachrichten/coronavirus/virusvariante-bereits-in-europa-nachgewiesen-was-%C3%BCber-die-indische-corona-mutante-b1617-bekannt-ist/ar-BB1fVPZy"
+    },
+    {
+        name:"Corona in Brasilien: Jetzt bloß nicht schwanger werden!",
+        image:"https://www.bing.com/th?id=OVFT.-aNkOeJlHS8XQMmtHSTsfC&amp;pid=News",
+        url:"https://www.msn.com/de-de/nachrichten/welt/corona-in-brasilien-jetzt-blo%C3%9F-nicht-schwanger-werden/ar-BB1fUCgF"
+    },
+    {
+        name:"Klimagipfel: Bidens Versprechen: Die USA wollen wieder Vorreiter beim Klimaschutz sein",
+        image:"https://www.bing.com/th?id=OVFT.BPrqS26BdeymOSOEhg2Z6C&pid=News",
+        url:"https://www.msn.com/de-de/nachrichten/politik/klimagipfel-bidens-versprechen-die-usa-wollen-wieder-vorreiter-beim-klimaschutz-sein/ar-BB1fWhYp"
+    },
+    {
+        name:"Impfungen für alle ab Juni?: Was das Ende der Priorisierung für die Impfkampagne bedeutet",
+        image:"https://https://www.bing.com/th?id=OVFT.A3Qcg7C9iBpEbeT6KFjwyi&pid=Newswww.bing.com/th?id=OVFT.BPrqS26BdeymOSOEhg2Z6C&pid=News",
+        url:"https://www.msn.com/de-de/gesundheit/medizinisch/impfungen-f%C3%BCr-alle-ab-juni-was-das-ende-der-priorisierung-f%C3%BCr-die-impfkampagne-bedeutet/ar-BB1fVL97"
+    },
+    {
+        name:"Impfungen in Arztpraxen freigegeben: Astrazeneca für alle in Berlin – aber nicht in Brandenburg",
+        image:"https://www.bing.com/th?id=OVFT.neAmtZGDwiZ4DU-6dL8XoS&pid=News",
+        url:"https://www.msn.com/de-de/nachrichten/politik/impfungen-in-arztpraxen-freigegeben-astrazeneca-f%C3%BCr-alle-in-berlin-aber-nicht-in-brandenburg/ar-BB1fVUx5"
+            },
+]
+export default news;
